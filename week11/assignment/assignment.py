6bca4f766f72758ca8146498a24ad48491f45292
Marshall Haymond <Marshallhay@msn.com>
2021-12-01 08:20:48 -0700

    just about done

--- week11/assignment/assignment.py
+++ week11/assignment/assignment.py
@@ -2,6 +2,7 @@
 Course: CSE 251
 Lesson Week: 11
 File: Assignment.py
+Marshall Haymond
 """
 
 import time
@@ -13,7 +14,7 @@ CLEANING_STAFF = 2
 HOTEL_GUESTS = 5
 
 # Run program for this number of seconds
-TIME = 60
+TIME = 5
 
 STARTING_PARTY_MESSAGE =  'Turning on the lights for the party vvvvvvvvvvvvvv'
 STOPPING_PARTY_MESSAGE  = 'Turning off the lights  ^^^^^^^^^^^^^^^^^^^^^^^^^^'
@@ -40,7 +41,7 @@ def guest_partying(id):
     time.sleep(random.uniform(0, 1))
 
 # -----------------------------------------------------------------------------
-def cleaner():
+def cleaner(id, start_time, sl, lock_c):
     """
     do the following for TIME seconds
     cleaner will wait to try to clean the room (cleaner_waiting())
@@ -49,10 +50,24 @@ def cleaner():
         Take some time cleaning (cleaner_cleaning())
         display message STOPPING_CLEANING_MESSAGE
     """
-    pass
+    
+    while True:
+        totaltime = round((time.time() - start_time), 2)
+        if totaltime >= TIME:
+            print(f"Party done for the cleaner {id}")
+            break
+
+        if sl[0] == 0:
+            print(STARTING_CLEANING_MESSAGE)
+            lock_c.acquire()
+            cleaner_cleaning(id)
+            sl[2] += 1
+            lock_c.release()
+            print(STOPPING_CLEANING_MESSAGE)
+
 
 # -----------------------------------------------------------------------------
-def guest():
+def guest(id, start_time, sl, lock_g):
     """
     do the following for TIME seconds
     guest will wait to try to get access to the room (guest_waiting())
@@ -61,18 +76,65 @@ def guest():
         Take some time partying (guest_partying())
         display message STOPPING_PARTY_MESSAGE if the guest is the last one leaving in the room
     """
-    pass
+    while True:
+        totaltime = round((time.time() - start_time), 2)
+        if totaltime >= TIME:
+            print(f'Party done for the guest {id}')
+            break
+
+        print(STARTING_PARTY_MESSAGE)
+        lock_g.acquire()
+        sl[0] += 1
+        guest_partying(id)
+        sl[1] += 1
+        lock_g.release()
+        sl[0] -= 1
+        print(STOPPING_PARTY_MESSAGE)
+
+
 
 # -----------------------------------------------------------------------------
 def main():
-    # TODO - add any variables, data structures, processes you need
-    # TODO - add any arguments to cleaner() and geust() that you need
 
     # Start time of the running of the program. 
     start_time = time.time()
+    #print(start_time)
+
+    # TODO - add any variables, data structures, processes you need
+    # TODO - add any arguments to cleaner() and geust() that you need
+    g_count = 0
+    clean_total = 0
+    party_total = 0
+
+
+
+    sl= mp.Manager().list()
+    sl.append(g_count)
+    sl.append(party_total)
+    sl.append(clean_total)
+
+    lock_c = mp.Lock()
+    lock_g = mp.Lock()
+
+    processes_c = [mp.Process(target=cleaner, args=(i,start_time, sl, lock_c)) for i in range(CLEANING_STAFF)]
+    processes_g = [mp.Process(target=guest, args=(i,start_time, sl, lock_g)) for i in range(HOTEL_GUESTS)]
+
+    #start
+    for i in range(CLEANING_STAFF):
+        processes_c[i].start()
+    
+    for i in range(HOTEL_GUESTS):
+        processes_g[i].start()
+
+    #join
+    for i in range(CLEANING_STAFF):
+        processes_c[i].join()
+    
+    for i in range(HOTEL_GUESTS):
+        processes_g[i].join()
 
     # Results
-    print(f'Room was cleaned {cleaned_count} times, there were {party_count} parties')
+    print(f'Room was cleaned {sl[2]} times, there were {sl[1]} parties')
 
 
 if __name__ == '__main__':
